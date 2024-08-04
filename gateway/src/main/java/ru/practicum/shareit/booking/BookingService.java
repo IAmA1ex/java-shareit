6a4b2a31b4efdd3e26ee7582ba54d13b14a1f320
IAmA1ex <alexander.mitevitch@yandex.ru>
2024-08-05 00:20:06 +0300

    feature: tests

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingService.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingService.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.booking;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingDtoShort;
 import ru.practicum.shareit.booking.dto.mapper.BookingDtoMapper;
 import ru.practicum.shareit.booking.dto.BookingState;
+import ru.practicum.shareit.client.ResponseHandler;
 
 import java.util.List;
 
@@ -20,60 +22,46 @@ public class BookingService {
 
     private final BookingClient bookingClient;
     private final BookingDtoMapper bookingDtoMapper;
+    private final ResponseHandler responseHandler;
 
     public BookingDto createBooking(BookingDtoShort bookingDtoShort, Long userRenterId) {
         log.info("GATEWAY: получен запрос на аренду вещи с id = {}.", bookingDtoShort.getItemId());
         BookItemRequestDto bookItemRequestDto = bookingDtoMapper.toBookItemRequestDto(bookingDtoShort);
         ResponseEntity<Object> response = bookingClient.bookItem(userRenterId, bookItemRequestDto);
-        if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
-            BookingDto bookingDto = (BookingDto) response.getBody();
-            log.info("GATEWAY: обработан запрос на аренду вещи с id = {}.", bookingDtoShort.getItemId());
-            return bookingDto;
-        }
-        return null; // !!!!!
+        BookingDto bookingDto = responseHandler.handleResponse(response, new TypeReference<BookingDto>() {});
+        log.info("GATEWAY: обработан запрос на аренду вещи с id = {}.", bookingDtoShort.getItemId());
+        return bookingDto;
     }
 
     public BookingDto approveBooking(Long bookingId, Long userId, Boolean approved) {
         log.info("GATEWAY: получен запрос на подтверждение аренды с id = {}.", bookingId);
         ResponseEntity<Object> response = bookingClient.handleBooking(bookingId, userId, approved);
-        if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
-            BookingDto bookingDto = (BookingDto) response.getBody();
-            log.info("GATEWAY: обработан запрос на подтверждение аренды с id = {}.", bookingId);
-            return bookingDto;
-        }
-        return null; // !!!!!
+        BookingDto bookingDto = responseHandler.handleResponse(response, new TypeReference<BookingDto>() {});
+        log.info("GATEWAY: обработан запрос на подтверждение аренды с id = {}.", bookingId);
+        return bookingDto;
     }
 
     public BookingDto getBooking(Long bookingId, Long userId) {
         log.info("GATEWAY: получен запрос на получение аренды с id = {}.", bookingId);
         ResponseEntity<Object> response = bookingClient.getBooking(userId, bookingId);
-        if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
-            BookingDto bookingDto = (BookingDto) response.getBody();
-            log.info("GATEWAY: обработан запрос на получение аренды c id = {}.", bookingId);
-            return bookingDto;
-        }
-        return null; // !!!!!
+        BookingDto bookingDto = responseHandler.handleResponse(response, new TypeReference<BookingDto>() {});
+        log.info("GATEWAY: обработан запрос на получение аренды c id = {}.", bookingId);
+        return bookingDto;
     }
 
     public List<BookingDto> getBookings(Long userId, BookingState state) {
-        log.info("GATEWAY: получен запрос на получение исходящих запросов на аренду (1).");
+        log.info("GATEWAY: получен запрос на получение всех запросов на аренду.");
         ResponseEntity<Object> response = bookingClient.getBookings(userId, state);
-        if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
-            List<BookingDto> bookingDtos = (List<BookingDto>) response.getBody();
-            log.info("GATEWAY: обработан запрос на получение исходящих запросов на аренду (1).");
-            return bookingDtos;
-        }
-        return null; // !!!!!
+        List<BookingDto> bookingDtos = responseHandler.handleResponse(response, new TypeReference<List<BookingDto>>(){});
+        log.info("GATEWAY: обработан запрос на получение всех запросов на аренду.");
+        return bookingDtos;
     }
 
     public List<BookingDto> getBookingsOwner(Long userId, BookingState state) {
-        log.info("GATEWAY: получен запрос на получение исходящих запросов на аренду (2).");
+        log.info("GATEWAY: получен запрос на получение исходящих запросов на аренду от пользователя.");
         ResponseEntity<Object> response = bookingClient.getBookingsOwner(userId, state);
-        if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
-            List<BookingDto> bookingDtos = (List<BookingDto>) response.getBody();
-            log.info("GATEWAY: обработан запрос на получение исходящих запросов на аренду (2).");
-            return bookingDtos;
-        }
-        return null; // !!!!!
+        List<BookingDto> bookingDtos = responseHandler.handleResponse(response, new TypeReference<List<BookingDto>>(){});
+        log.info("GATEWAY: обработан запрос на получение исходящих запросов на аренду пользователя.");
+        return bookingDtos;
     }
 }
